e51cc01fc53cb836b9a825ee080b80a341afd513
indraadp <indraproject777@gmail.com>
2023-08-12 13:56:26 +0700

    lime: SuperiorOS Android 13: collecting ccache
    Signed-off-by: indraadp <indraproject777@gmail.com>

--- collect.sh
+++ collect.sh
@@ -10,10 +10,10 @@ ccache -M 50G -F 0
 ccache -o compression=true
 ccache -z
 export BUILD_USERNAME="IndraDP"
-export BUILD_HOSTNAME="idev"
+export BUILD_HOSTNAME="iDev"
 export WITHOUT_CHECK_API=true
-lunch nad_juice-userdebug
-mka nad &
+lunch superior_lime-userdebug
+m bacon &
 sleep 97m
 kill %1
 ccache -s
